a841df1a317f3855541232aee6aa37a12a77d5f7
Ege YourLastName <hello@egeuysal.com>
2025-05-29 21:49:16 +0300

    Add docker configuration

--- frontend/src/lib/utils.ts
+++
@@ -1,6 +0,0 @@
-import { clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
-
-export function cn(...inputs: (string | undefined | null | false | Record<string, unknown>)[]) {
-  return twMerge(clsx(inputs));
-}
